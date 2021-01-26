0c35511e52491c96ae3c046d4bedf5b41a219947
davide <davidecorsinimilano@gmail.com>
2021-01-26 15:28:56 +0100

    working  on

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let app = new Vue({
+    el: '#app',
+    data:{
+        timerTitle: false,
+        images: [
+            'https://cdn.dribbble.com/users/43762/screenshots/1438973/ng-colab-space_day.gif',
+            'https://i.pinimg.com/originals/2c/5e/5b/2c5e5b2046de05ff3f3e28fbe1b7d169.gif',
+            'https://www.domusweb.it/content/dam/domusweb/it/speciali/domusfordesign/2020/il-nuovo-design-italiano/domus-for-design-annichiarico-bianchi-03.jpg.foto.rmedium.jpg',
+            'https://i.pinimg.com/originals/e4/d6/68/e4d668d1400a3a4541f861cb383074e6.gif',
+            'https://i.stack.imgur.com/Dil7r.gif',
+            'https://media3.giphy.com/media/LwzDBXlwVOGz7zi8cp/giphy.gif',
+            'https://www.weather.gov/images/jetstream/clouds/prism.gif',
+            'https://www.edmundoptics.com/globalassets/knowledge-center/app-notes/optics/intro-polarization-fig-2a-new.gif'
+        ]
+
+    },
+    methods: {
+
+        titleTime(){
+            
+            this.timerTitle = !this.timerTitle;
+        }
+    }
+});
