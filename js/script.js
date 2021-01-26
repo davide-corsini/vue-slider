9a7251916aca68cf02c9fc0fcdd603a185f89700
davide <davidecorsinimilano@gmail.com>
2021-01-26 21:37:27 +0100

    working on

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let app = new Vue({
     el: '#app',
     data:{
         timerTitle: false,
+        imgBig: 'big',
         images: [
             'https://cdn.dribbble.com/users/43762/screenshots/1438973/ng-colab-space_day.gif',
             'https://i.pinimg.com/originals/2c/5e/5b/2c5e5b2046de05ff3f3e28fbe1b7d169.gif',
@@ -10,10 +11,25 @@ let app = new Vue({
             'https://i.stack.imgur.com/Dil7r.gif',
             'https://media3.giphy.com/media/LwzDBXlwVOGz7zi8cp/giphy.gif',
             'https://miro.medium.com/max/1600/1*kiAtJzGH6tcKnILUKJenvA.gif',
-            'https://s3.amazonaws.com/blog.invisionapp.com/uploads/2017/04/running-gif.gif'
+            'https://s3.amazonaws.com/blog.invisionapp.com/uploads/2017/04/running-gif.gif',
+            'https://33.media.tumblr.com/c414f6e92d4ca4229080483543afcc27/tumblr_nwj4jxKySK1qgzs6bo1_540.gif',
+            'https://i.gifer.com/G4H1.gif',
+            'https://vitalvernici.it/wp-content/uploads/2020/10/city-illustration-motion-design.gif',
+            'https://miro.medium.com/max/1600/1*Ig4t1nwU_RTQAnMym1tURg.gif',
+            'https://joyenergizer.com/wp-content/uploads/2017/01/GIFS-21.gif',
+            'https://freefrontend.com/assets/img/css-animation-examples/css-handbook-download-animation.gif',
+            'https://media4.giphy.com/media/xUOwG9fkX7kINUrIw8/giphy.gif',
+            'https://i.pinimg.com/originals/c3/66/af/c366af06de9d81af7adf8188ce872734.gif'
         ],
-        countImg: 0
+        countImg: 0,
+        count2: 1,
+        count3: 2,
+        rotationText: 'pippo',
+        interval: ''
 
+    },
+    created(){
+        this.interval = setInterval( this.nextImg,3000 );
     },
     methods: {
 
@@ -30,9 +46,37 @@ let app = new Vue({
             else{
                 this.countImg++;
             }
+
+            if (this.count2 == (this.images.length - 1)) {
+                this.count2 = 1;
+            }
+            else {
+                this.count2++;
+            }
+
+            if (this.count3 == (this.images.length - 1)) {
+                this.count3 = 2;
+            }
+            else {
+                this.count3++;
+            }
+            
         },
         prevImg(){
             this.countImg--;
+            (this.countImg < 0) ? this.countImg = (this.images.length-1) : this.countImg --;
+            this.count2--;
+            (this.count2 < 1) ? this.count2 = (this.images.length - 2) : this.count2--;
+            this.count3--;
+            (this.count3 < 2) ? this.count3 = (this.images.length - 3) : this.count3--;
+        },
+        changeImg(indice){
+            this.count2 = indice;
+            clearInterval(this.interval);
+        },
+        ruotaTesto(){
+            (this.rotationText == '') ? this.rotationText = '' : this.rotationText = 'grades';
         }
+
     }
 });
